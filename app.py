b592be37435c37c4e3be6c503290b0bf8614b955
Xi Zhang <49449649+xzhangfox@users.noreply.github.com>
2021-05-18 22:06:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,20 +12,6 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-#To use the predict button in our web-app
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = {'predicted': prediction[0]}
-
-    return render_template('index.html', prediction_text=output)
-
 @app.route('/processjson', methods = ['POST'])
 def processjson():
     data = request.get_json()
